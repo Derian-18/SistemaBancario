c82111761084da8b283ebe5051e067a2c88c4ec0
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-08-31 23:13:29 -0700

    feat(Model): Atributos para las tablas en mysql agregadas

---
+++ src/main/java/com/Springboot/aplicationweb/Usuario/Model/Usuarios.java
@@ -0,0 +1,27 @@
+package com.Springboot.aplicationweb.Usuario.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Esto es lo que va a llevar mi base de datos
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer usuarioId;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String  nombre;
+
+    @Column(name = "apellido", nullable = false, length = 100)
+    private String apellido;
+
+    @Column(name = "correo", nullable = false, unique = true, length = 150)
+    private String correo;
+}
