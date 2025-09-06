92dd32a2513bb75592af2685224316eb965b33b8
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-09-05 19:06:26 -0700

    feat(Controllers): Se agregaron metodos para CRUD cencillo

---
+++ src/main/java/com/Springboot/aplicationweb/Usuario/Controllers/UsuarioControlador.java
@@ -0,0 +1,79 @@
+package com.Springboot.aplicationweb.Usuario.Controllers;
+import com.Springboot.aplicationweb.Usuario.Dto.UsuarioCreateDTO;
+import com.Springboot.aplicationweb.Usuario.Dto.UsuarioResponseDTO;
+import com.Springboot.aplicationweb.Usuario.Dto.UsuarioUpdateDTO;
+import com.Springboot.aplicationweb.Usuario.Model.Usuarios;
+import com.Springboot.aplicationweb.Usuario.Servicios.UsuarioService;
+import jakarta.persistence.EntityNotFoundException;
+import jdk.jfr.Description;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioControlador {
+
+    @Autowired
+    private UsuarioService serv_usuario;
+
+    // Aquí se puede usar la clase DTO o usar ResponseEntity o usar los dos al mimso tiempo
+
+    // Este GetMapping hace que se muestren todos los usuarios mediante el ResponseDTO
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listarTodos(){
+        List<UsuarioResponseDTO> usuarios = serv_usuario.ListaUsuarios().stream()
+                .map(u -> new UsuarioResponseDTO(u.getUsuarioId(), u.getNombre(), u.getApellido(), u.getCorreo())).toList();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    // Aqui buscamos un usuario por el id
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> buscar(@PathVariable int id){
+        return serv_usuario.BuscarId(id)
+                .map(usuario -> new UsuarioResponseDTO(
+                        usuario.getUsuarioId(),
+                        usuario.getNombre(),
+                        usuario.getApellido(),
+                        usuario.getCorreo()
+                ))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Aqui crearemos un usuario
+    @PostMapping
+    public ResponseEntity<UsuarioCreateDTO> crearUsuario(@RequestBody UsuarioCreateDTO usuariodto){
+        Usuarios insertar = serv_usuario.Crear(usuariodto);
+        UsuarioCreateDTO respDTO = new UsuarioCreateDTO(
+                insertar.getNombre(),
+                insertar.getApellido(),
+                insertar.getCorreo());
+        return ResponseEntity.status(HttpStatus.CREATED).body(respDTO);
+    }
+
+    // Aqui actualizaremos un usuario
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioUpdateDTO> actualizarUsuario(@PathVariable int id, @RequestBody UsuarioUpdateDTO usuariodto){
+        return serv_usuario.Actualizar(id, usuariodto)
+                .map(u -> new UsuarioUpdateDTO(u.getNombre(), u.getApellido(),u.getCorreo()))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    // Aqui eliminaremos un usuario
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> borrarUsuario(@PathVariable int id){
+        try{
+            serv_usuario.Eliminar(id);
+            return ResponseEntity.noContent().build();
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
