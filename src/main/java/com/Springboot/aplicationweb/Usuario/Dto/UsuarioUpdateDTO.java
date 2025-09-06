9b951fb463a854b9bddb4603958b990a63acac1e
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-09-05 20:10:22 -0700

    feat(DTO): Se creo esta clase para actualizar datos de usuarios

---
+++ src/main/java/com/Springboot/aplicationweb/Usuario/Dto/UsuarioUpdateDTO.java
@@ -0,0 +1,30 @@
+package com.Springboot.aplicationweb.Usuario.Dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioUpdateDTO {
+
+    @NotNull(message = "Solo esta permitido ingresar letras")
+    @Pattern(regexp = "^[A-Za-z\\sáéíóúÁÉÍÓÚñÑ]+$")
+    @NotBlank(message = "El nombre es obligatorio")
+    private String  nombre;
+
+    @NotNull
+    @NotBlank(message = "El apelliddo es obligatorio")
+    @Pattern(regexp = "^[A-Za-z\\sáéíóúÁÉÍÓÚñÑ]+$")
+    private String apellido;
+
+    @NotNull
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "El email debe estar escrito correctamente")
+    private String correo;
+}
