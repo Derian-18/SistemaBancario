d09bc319141f6183c7ec8bbc9e8135fd7f11a48b
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-09-10 23:31:26 -0700

    feat(Repositorio): Se agregaron metodos para verificar que existe email y buscar por email

--- src/main/java/com/Springboot/aplicationweb/Usuario/Repositorios/UsuariosRepositorio.java
+++ src/main/java/com/Springboot/aplicationweb/Usuario/Repositorios/UsuariosRepositorio.java
@@ -4,8 +4,16 @@ import com.Springboot.aplicationweb.Usuario.Model.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // Aquí es donde ya se conecta a la base de datos
 @Repository
 public interface UsuariosRepositorio extends JpaRepository<Usuarios, Integer>{
+
+    // Metodo para buscar por email
+    Optional<Usuarios> findByCorreo(String correo);
+
+    // Metodo para verificar si existe por email
+    boolean existsByCorreo(String correo);
 
 }
