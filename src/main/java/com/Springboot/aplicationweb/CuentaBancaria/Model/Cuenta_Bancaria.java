765d9f26dac63c0d0a7c01a540bf932bc47d1da8
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-09-23 20:22:06 -0700

    feat(CuentaBancaria(Model)): Se agregaron los datos de entrada para las cuentas bancarias

--- src/main/java/com/Springboot/aplicationweb/CuentaBancaria/Model/Cuenta_Bancaria.java
+++ src/main/java/com/Springboot/aplicationweb/CuentaBancaria/Model/Cuenta_Bancaria.java
@@ -1,4 +1,31 @@
-package com.Springboot.aplicationweb.CuentaBancaria;
+package com.Springboot.aplicationweb.CuentaBancaria.Model;
 
-public class fdf {
+import com.Springboot.aplicationweb.Usuario.Model.Usuarios;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "cuenta_bancaria")
+public class Cuenta_Bancaria {
+    // Esta es la base de datos para la cuanta bancaria de cada usuario registrado
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cuenta_id;
+
+    // Esta es la llave foránea
+    @ManyToOne
+    @JoinColumn(name = "usuarioId", nullable = false)
+    private Usuarios usuarioId;
+
+    @Column(name = "numeroCuenta", nullable = false)
+    private String numeroCuenta;
+
+    @Column(name = "saldo", nullable = false)
+    private BigDecimal saldo;
 }
