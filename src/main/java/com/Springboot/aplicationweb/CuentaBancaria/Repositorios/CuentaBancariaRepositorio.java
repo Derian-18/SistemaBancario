b2e670a3b5e3d482b90044b027375046bcff00fb
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2025-10-04 22:38:45 -0700

    feat(CuentaBancaria(Repositorios)): Se agrega  conexion a la base de datos

--- src/main/java/com/Springboot/aplicationweb/CuentaBancaria/Repositorios/CuentaBancariaRepositorio.java
+++ src/main/java/com/Springboot/aplicationweb/CuentaBancaria/Repositorios/CuentaBancariaRepositorio.java
@@ -2,8 +2,10 @@ package com.Springboot.aplicationweb.CuentaBancaria.Repositorios;
 
 import com.Springboot.aplicationweb.CuentaBancaria.Model.Cuenta_Bancaria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // Aquí se conectará a la base de datos de la cuenta bancaria
+@Repository
 public interface CuentaBancariaRepositorio extends JpaRepository<Cuenta_Bancaria, Integer> {
 
 }
